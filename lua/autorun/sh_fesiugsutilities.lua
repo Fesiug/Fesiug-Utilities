7c1a31e0e492df0878ff76c937a599bf6348e445
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-06-23 03:29:36 +0300

    this works well and fine
    as expected, even

--- lua/autorun/sh_fesiugsutilities.lua
+++ lua/autorun/sh_fesiugsutilities.lua
@@ -120,24 +120,25 @@ hook.Add( "EntityTakeDamage", "YouWillFuckNPCs", function( target, dmginfo )
 	dmginfo:SetDamage( dmg * mult )
 
 	if FES_GC("fes_plymod_abarmor", "b") and target:IsPlayer() and target:Armor() > 0 then
-		local m, d, a, h, acc = FES_GC("fes_plymod_abarmor", "i"), dmginfo:GetDamage(), target:Armor(), target:Health(), target:GetInternalVariable("m_flDamageAccumulator")
+		local d, a, h, acc = dmginfo:GetDamage(), target:Armor(), target:Health(), target:GetInternalVariable("m_flDamageAccumulator")
+		local isfall = (FES_GC("fes_plymod_abarmor_fall", "b") and dmginfo:IsDamageType(DMG_FALL) and dmginfo:GetInflictor():IsWorld())
+		-- print(isfall, !dmginfo:IsDamageType(DMG_FALL+DMG_DROWN+DMG_RADIATION+DMG_POISON), target:Armor(), d * 0.8, target:Armor() > d * 0.8)
 		-- Can't I just modify the fucking damage forces we take? This is all a bodge just to make that work
-		if  (!dmginfo:IsDamageType(DMG_FALL+DMG_DROWN+DMG_RADIATION+DMG_POISON) or (FES_GC("fes_plymod_abarmor_fall", "b") and dmginfo:GetDamageType() == DMG_FALL and dmginfo:GetInflictor():IsWorld())) then -- DMG_DIRECT bypasses any damage scaling (i.e. Alien Grunts' armor, Barney helmet), but is NOT directly applied to a player's health pool
+		if isfall or !dmginfo:IsDamageType(DMG_FALL+DMG_DROWN+DMG_RADIATION+DMG_POISON) and target:Armor() > d then -- DMG_DIRECT bypasses any damage scaling (i.e. Alien Grunts' armor, Barney helmet), but is NOT directly applied to a player's health pool
 			dmginfo:SetDamageBonus(d)
-			if m == 1 then
+			if FES_GC("fes_plymod_abarmor", "i") == 1 then
 				dmginfo:SetDamageType(bit.bor(dmginfo:GetDamageType(), DMG_FALL)) -- can't we just apply damage directly in some more sensible way than this? DMG_DIRECT bypasses ScaleDamage, is not direct damage to player
 				target:SetHealth(math.floor(math.max(h + d + math.min(a - d, 0) + acc, 0))) -- so we don't actually die of too much damage in the first place, we can't call it post-taken
-			elseif target:Armor() >= d then -- simpler form of ablation, but no knockback, will take 1 more armor damage as this is stock gmod behavior and i dont feel like compensating. otherwise more compatible as it 100% works with insane stats like above does
-				dmginfo:ScaleDamage(1-math.Clamp(d != 0 and target:Armor() / d or 0, 0, 1))
+			else -- simpler form of ablation, but no knockback, and will take 1 more armor damage as this is stock gmod behavior and i dont feel like compensating. otherwise more compatible as it 100% works with insane stats like above does
+				dmginfo:ScaleDamage(1-math.Clamp(d > 0 and target:Armor() / d or 0, 0, 1))
 			end
 		end
 	end
 end )
 hook.Add("PostEntityTakeDamage", "DamageTaken", function(target, dmginfo, took)
-	print(dmginfo:GetDamage())
 	if FES_GC("fes_plymod_abarmor", "b") and target:IsPlayer() and target:Armor() > 0 then
 		local b = dmginfo:GetDamageBonus()
-		if (!FES_GC("fes_plymod_abarmor_fall", "b") and dmginfo:GetDamageType() == DMG_FALL and dmginfo:GetInflictor():IsWorld()) then return end
+		if b <= 1 or (!FES_GC("fes_plymod_abarmor_fall", "b") and dmginfo:GetDamageType() == DMG_FALL and dmginfo:GetInflictor():IsWorld()) then return end
 		target:SetArmor(math.max(target:Armor() - b, 0)) -- calling here for responsiveness, hitmarkers 
 	end
 end)
